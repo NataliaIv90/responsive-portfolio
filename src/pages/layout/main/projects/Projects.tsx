d8542b1f5b8350b13274c67bd0788c622c909a9f
Natalia Ivantsova <ivantsovana@gmail.com>
2023-10-13 14:03:06 +0300

    Update Projects.tsx

--- src/pages/layout/main/projects/Projects.tsx
+++ src/pages/layout/main/projects/Projects.tsx
@@ -14,6 +14,16 @@ const projectsData: IProjectCardProps[] = [
     usedTechnologies: [ETechOptions.thtml, ETechOptions.tcss, ETechOptions.tjs]
 
   },
+  {
+    imageSrc: 'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+    imageAlt: 'Alt',
+    title: 'Card',
+    text: 'Text',
+    field: 'Fiesl',
+    buttonLink: 'Link',
+    usedTechnologies: [ETechOptions.tfigma, ETechOptions.tmui, ETechOptions.tjs]
+
+  },
 ]
 
 const Projects = () => {
