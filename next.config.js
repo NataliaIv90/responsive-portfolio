f3cc2dde4f0df1b67cf4eda5739a93fd441421a0
Natalia Ivantsova <ivantsovana@gmail.com>
2023-10-13 11:58:14 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['.tsx'],
+  pageExtensions: ['tsx', 'mdx'],
+  experimental: {
+    mdxRs: true,
+  },
 }
 
 module.exports = nextConfig
